64754c0ba92e131913f4d389c2103b615684d72a
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-04-12 08:48:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
 from hdbcli import dbapi
 import pandas as pd
-
+import json
 app = Flask(__name__)
 @app.route("/")
 def hello():
     events = request.args.get('event')
-    return '''<h1> Below Events logs are:</h1> <br><br> {}'''.format(events)
+    map = json.loads(events)
+    key = map["documentInput"]["itemInput"]["attributes"][10]["name"]
+    keyval = map["documentInput"]["itemInput"]["attributes"][10]["values"]
+    return '''<h1> Below Events logs are:</h1> <br><br> key: {} :: keyVal:{}'''.format(key,keyval)
 
 
 if __name__ == "__main__":
